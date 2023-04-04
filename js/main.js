6b14b97f1387e276bf2f82465e9b00d939b29e4f
Matheus Silva <scruz.math@gmail.com>
2023-04-04 20:33:23 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens = []
+
+form.addEventListener("submit", (evento)=> {
+    evento.preventDefault()
+
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+    criaElemento(nome.value, quantidade.value)
+
+    nome.value = ""
+    quantidade.value = ""
+})
+
+function criaElemento(nome, quantidade){
+
+    //<li class="item"><strong>7</strong>Camisas</li>
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+    
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += nome
+
+    lista.appendChild(novoItem)
+
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("item", JSON.stringify(itens))
+}
